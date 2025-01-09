b3e32f3313dcc028029c65170c3f276abb9edbbc
Chelsea.Y <yangqingyan0@gmail.com>
2025-01-09 21:24:08 +1030

    remove test sidebars

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Welcome to My App</h1>
-      <p>This is the home page content.</p>
-    </div>
-  );
-}
