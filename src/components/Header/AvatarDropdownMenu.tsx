f0e73c505e63349ea497c2b5ba226dfb2b32f7b0
Chelsea Yang <100550092+qingyanyang@users.noreply.github.com>
2025-01-12 13:53:26 +1030

    feat: add avator dropdown menu (#12)

---
+++ src/components/Header/AvatarDropdownMenu.tsx
@@ -0,0 +1,25 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { ReactNode } from "react";
+
+const AvatarDropdownMenu: React.FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuGroup>
+          <DropdownMenuItem>My profile</DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Log out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+export default AvatarDropdownMenu;
