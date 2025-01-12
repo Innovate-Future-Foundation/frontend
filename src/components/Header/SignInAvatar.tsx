e49f4b8d4103137fe30db0ac827d890f9ac86b23
Chelsea.Y <yangqingyan0@gmail.com>
2025-01-12 14:39:29 +1030

    refactor: apply App Avatar

--- src/components/Header/SignInAvatar.tsx
+++ src/components/Header/SignInAvatar.tsx
@@ -1,5 +1,5 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { abbreviateName } from "@/utils/formatters";
+import AppAvatar from "../AppAvatar";
 
 type avatarType = {
   roleName: string;
@@ -8,20 +8,13 @@ type avatarType = {
   avatarLink: string;
 };
 const SignInAvatar: React.FC<avatarType> = ({ roleName, name, email, avatarLink }) => {
-  const avatarPlaceHolder = abbreviateName(name);
-  const nameString = `${roleName}: ${name}`;
   return (
     <div className="flex gap-2 items-center">
       <div className="flex flex-col items-end gap-1">
-        <p className="text-primary font-bold text-sm leading-3">{nameString}</p>
+        <p className="text-primary font-bold text-sm leading-3">{`${roleName}: ${name}`}</p>
         <p className="text-muted-foreground text-sm leading-3">{email}</p>
       </div>
-      <div className="w-10 h-10">
-        <Avatar className="w-full h-full">
-          <AvatarImage src={avatarLink} />
-          <AvatarFallback>{avatarPlaceHolder}</AvatarFallback>
-        </Avatar>
-      </div>
+      <AppAvatar avatarLink={avatarLink} avatarAlt={"InnovateFuture"} avaterPlaceholder={abbreviateName(name)} />
     </div>
   );
 };
