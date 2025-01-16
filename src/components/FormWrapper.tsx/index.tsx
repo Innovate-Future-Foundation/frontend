58fd0ebb9d849557521022de3fe2db84d527e1bf
Chelsea.Y <yangqingyan0@gmail.com>
2025-01-17 01:05:25 +1030

    feat: add validation; encapsulate formfield

--- src/components/FormWrapper.tsx/index.tsx
+++ src/components/FormWrapper.tsx/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import { Edit } from "lucide-react";
+import { Save } from "lucide-react";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
@@ -7,21 +7,24 @@ import { Button } from "@/components/ui/button";
 interface FormPieceProp {
   formTitle: string;
   children: ReactNode;
+  onSave: () => void;
 }
 
-const FormWrapper: React.FC<FormPieceProp> = ({ formTitle, children }) => {
+const FormWrapper: React.FC<FormPieceProp> = ({ formTitle, children, onSave }) => {
   return (
     <Card className="rounded-md shadow-none w-full overflow-hidden">
       <CardHeader>
         <div className="flex justify-between items-center">
           <CardTitle className="text-lg">{formTitle}</CardTitle>
-          <Button variant={"outline"} className="rounded-full">
-            Edit <Edit />
-          </Button>
+          <div className="flex gap-4">
+            <Button className="rounded-full" onClick={onSave}>
+              Save <Save />
+            </Button>
+          </div>
         </div>
       </CardHeader>
       <CardContent>
-        <form className="flex flex-col gap-4 pb-8">{children}</form>
+        <form className="flex flex-col gap-4 pb-4">{children}</form>
       </CardContent>
     </Card>
   );
