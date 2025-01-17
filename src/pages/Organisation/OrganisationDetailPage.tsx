030dd8c8d9a23f0cc588aaaa20c5dd4255eebbb3
Raymond <zhuyuleiraymond@outlook.com>
2025-01-17 11:32:40 +1100

    refactor: optimize ScrollList layout and placement

--- src/pages/Organisation/OrganisationDetailPage.tsx
+++ src/pages/Organisation/OrganisationDetailPage.tsx
@@ -1,5 +1,13 @@
+import { ScrollList } from "@/components/ScrollList";
+
 const OrganisationDetailPage = () => {
-  return <div>OrganisationDetailPage</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="border bg-white p-8 min-h-[200px]">Organisation Details Component (Coming Soon)</div>
+
+      <ScrollList />
+    </div>
+  );
 };
 
 export default OrganisationDetailPage;
