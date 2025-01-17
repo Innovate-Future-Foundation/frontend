b5d9bd6512b780fa6a79ab7cbf7a628589c60119
Chelsea.Y <yangqingyan0@gmail.com>
2025-01-17 14:02:17 +1030

    feat: centralize dashboard menu data manangement

--- src/components/ScrollList/ScrollItem.tsx
+++ src/components/ScrollList/ScrollItem.tsx
@@ -1,7 +1,7 @@
 import { Trash2 } from "lucide-react";
 
 import { abbreviateName } from "@/utils/formatters";
-import AppAvatar from "../Avatar";
+import Avatar from "../Avatar";
 import { Button } from "../ui/button";
 
 interface ScrollItemProps {
@@ -13,7 +13,7 @@ interface ScrollItemProps {
 const ScrollItem = ({ name, email, avatarLink }: ScrollItemProps) => (
   <div className="flex items-center justify-between py-3 px-4 hover:bg-gray-50">
     <div className="flex items-center gap-3">
-      <AppAvatar avatarLink={avatarLink} avatarAlt={"InnovateFuture"} avatarPlaceholder={abbreviateName(name)} />
+      <Avatar avatarLink={avatarLink} avatarAlt={"InnovateFuture"} avatarPlaceholder={abbreviateName(name)} />
       <div>
         <p className="font-medium">{name}</p>
         <p className="text-sm text-gray-500">{email}</p>
--- src/pages/Dashboard/Sidebar.tsx
+++ src/pages/Dashboard/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Building2 } from "lucide-react";
+import { LucideIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 
 import {
@@ -11,27 +11,28 @@ import {
   SidebarMenuItem
 } from "@/components/ui/sidebar";
 
-const items = [
-  {
-    title: "Organisations",
-    url: "/dashboard",
-    icon: Building2
-  }
-];
+export interface SidebarItem {
+  title: string;
+  url: string;
+  icon: LucideIcon;
+}
+export interface SidebarProps {
+  sidebarItems: SidebarItem[];
+}
 
-const Sidebar = () => {
+const Sidebar: React.FC<SidebarProps> = ({ sidebarItems }) => {
   return (
     <CNSidebar collapsible="icon" variant="sidebar" className="mt-12">
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map(item => (
+              {sidebarItems.map(item => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
                     <Link to={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
+                      <item.icon className="w-5 h-5" />
+                      <span className="capitalize font-medium text-sm">{item.title}</span>
                     </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
