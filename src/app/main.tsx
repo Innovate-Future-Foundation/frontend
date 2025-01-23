83926c9d7be17c01eb4c0854469788dfa9814195
Chelsea Yang <100550092+qingyanyang@users.noreply.github.com>
2025-01-23 22:42:29 +1030

    feat: config toast (#38)

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,9 +2,13 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
+import { Toaster } from "@/components/ui/toaster";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
-  <App />
+  <>
+    <App />
+    <Toaster />
+  </>
   // </StrictMode>
 );
