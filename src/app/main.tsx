995b86188caf9ed2bb8db564dd4db2c27d8bb9fa
Chelsea.Y <yangqingyan0@gmail.com>
2025-01-24 23:22:25 +1030

    feat: add theme mode

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,13 +2,9 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
-import { Toaster } from "@/components/ui/toaster";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
-  <>
-    <App />
-    <Toaster />
-  </>
+  <App />
   // </StrictMode>
 );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 // import { COGNITO_LOGIN_URL } from "@/constants/apiConfig";
 import SignInAvatar from "./SignInAvatar";
+import { ModeToggle } from "../ui/mode-toggle";
 
 const Header = () => {
   return (
@@ -17,7 +18,10 @@ const Header = () => {
         {/* <a href={COGNITO_LOGIN_URL}>
           <p>Sign in</p>
         </a> */}
-        <SignInAvatar name={"John Doe"} email={"johndoe@example.com"} avatarLink={"https://github.com/davidmiller.png"} />
+        <div className="flex justify-items-center gap-4">
+          <ModeToggle />
+          <SignInAvatar name={"John Doe"} email={"johndoe@example.com"} avatarLink={"https://github.com/davidmiller.png"} />
+        </div>
       </nav>
     </header>
   );
