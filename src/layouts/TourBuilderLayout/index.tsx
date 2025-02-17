80a03574c7f2375db686140eab996c71d4586efc
Chelsea.Y <yangqingyan0@gmail.com>
2025-02-17 13:00:19 +1030

    feat: add just padding for tour builder layout

--- src/layouts/TourBuilderLayout/index.tsx
+++ src/layouts/TourBuilderLayout/index.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { FC, ReactNode } from "react";
 
 export interface TourBuilderLayout {
@@ -8,10 +11,24 @@ export interface TourBuilderLayout {
 
 const TourBuilderLayout: FC<TourBuilderLayout> = ({ children, title, subTitle }) => {
   return (
-    <div className="p-4 px-6">
-      <p className="font-bold text-2xl text-foreground/80 mb-1">{title}</p>
-      <p className="font-normal text-foreground/60">{subTitle}</p>
-      {children}
+    <div>
+      <p className="font-bold text-2xl text-foreground/80 mb-1 pt-6 px-6">{title}</p>
+      <p className="font-normal text-foreground/60 px-6">{subTitle}</p>
+      <div className="h-[500px] overflow-y-scroll px-6">{children}</div>
+      <Separator />
+      <div className="h-20 w-full flex justify-between items-center px-10">
+        <Button variant={"secondary"} className="px-12">
+          <div className="flex gap-2 items-center text-lg">
+            <ChevronLeft /> Back
+          </div>
+        </Button>
+        <Button variant={"default"} className="px-12 text-lg">
+          <div className="flex gap-2 items-center text-lg">
+            Next
+            <ChevronRight />
+          </div>
+        </Button>
+      </div>
     </div>
   );
 };
