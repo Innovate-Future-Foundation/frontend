0a62a011947816adaceaf36fdfefee332806acad
Chelsea.Y <yangqingyan0@gmail.com>
2025-02-24 20:50:43 +1030

    feat: add mask for login

--- src/pages/Authentication/index.tsx
+++ src/pages/Authentication/index.tsx
@@ -7,18 +7,25 @@ const AuthenticationPage = () => {
   console.log("location", location);
 
   return (
-    <div className="flex min-h-screen relative overflow-hidden">
+    <div className="flex h-screen overflow-hidden py-10 px-20">
       <motion.div
         key="blue-section"
         initial={{ x: "-100%" }}
         animate={{ x: param === "register" ? "190%" : "0" }}
         transition={{ duration: 0.8, ease: [0.25, 0.1, 0.25, 1.1] }}
-        className="hidden lg:flex fixed w-[35%] h-full flex-col bg-primary p-12 pt-16 text-white z-10"
+        className="fixed hidden lg:flex rounded-IF32px w-[calc(50vw-5rem-2rem)] h-[calc(100vh-5rem)] flex-col text-white"
+        style={{ maskImage: `url('/assets/images/mask.png')`, maskRepeat: `no-repeat`, maskSize: `100% 100%` }}
       >
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.5 }} className="mb-24">
+        <img src="/assets/images/sample.png" alt="sample" className="min-w-[500px]" />
+        {/* <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.5 }} className="mb-24">
           <h1 className="text-2xl font-bold">Company Name</h1>
-        </motion.div>
-
+        </motion.div> */}
+        {/* <div className="absolute top-0 right-0">
+          <SvgRightTopMask width={"50%"} height={"50%"} />
+        </div>
+        <div className="absolute bottom-0 left-0">
+          <SvgLeftBottomMask width={"50%"} height={"50%"} />
+        </div> */}
         <motion.div
           initial="hidden"
           animate="visible"
@@ -30,7 +37,7 @@ const AuthenticationPage = () => {
             }
           }}
         >
-          <motion.h2
+          {/* <motion.h2
             variants={{
               hidden: { opacity: 0, x: -20 },
               visible: {
@@ -83,10 +90,9 @@ const AuthenticationPage = () => {
 
           <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.8, delay: 1 }} className="text-base leading-relaxed">
             All-in-one solution to fix your business in the state. Form a new company from scratch or onboard your existing US company
-          </motion.p>
+          </motion.p> */}
         </motion.div>
       </motion.div>
-
       <div className="flex-1 relative">
         <div className="absolute inset-0">
           <AnimatePresence mode="wait">
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,8 @@ export default {
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
+				sm: 'calc(var(--radius) - 4px)',
+				IF32px: '1.6rem',
 			},
 			colors: {
 				background: 'hsl(var(--background)/ <alpha-value>)',
